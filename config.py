49fda25a2cd6f68baa7e33d70d044e80d6c53888
adhayankumar740-lgtm <adhayankumar740@gmail.com>
2025-09-25 20:23:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SESSION_NAME = os.getenv("SESSION_NAME")  # Userbot session string
+
+# alias so that old code using config.SESSION works
+SESSION = SESSION_NAME
